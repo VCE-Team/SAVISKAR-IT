b59aba939d0bc2aef04b79d5e70835ebac701ece
Sidhartha Reddy <kattasidharthareddy22it@student.vardhaman.org>
2024-10-30 07:37:25 +0530

    some changes

--- src/components/Hackathon/Participate.jsx
+++ src/components/Hackathon/Participate.jsx
@@ -19,7 +19,7 @@ const Participate = () => {
         </div>
         <div className='p-10 md:px-32'>
             <div className='border p-5 border-3 rounded-2xl'>
-            <p className='md:text-3xl text-xl text-white'>[Hackathon Name] is open to all B.Tech students who are ready to take on the challenge! Whether you’re in your first year or final year, if you’re passionate about technology and eager to innovate, you’re invited to join us.
+            <p className='md:text-3xl text-xl text-white'>Saviskar 2K24 is open to all B.Tech students who are ready to take on the challenge! Whether you’re in your first year or final year, if you’re passionate about technology and eager to innovate, you’re invited to join us.
             </p>
             <br />
             <p className='md:text-3xl text-xl text-white'>Bring your coding skills, creativity, and problem-solving mindset and get ready to collaborate with fellow students, develop groundbreaking solutions, and make an impact</p>
